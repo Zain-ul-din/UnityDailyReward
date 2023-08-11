b9cf524ad5331dd7cf13a0f2f6521d71757dceab
Zain_ul_din <78583049+Zain-ul-din@users.noreply.github.com>
2023-08-11 20:20:31 +0500

    Update DailyRewardConfigSO.cs

--- Scripts/SO/DailyRewardConfigSO.cs
+++ Scripts/SO/DailyRewardConfigSO.cs
@@ -10,13 +10,19 @@ namespace Randoms.DailyReward
         public string timerTextPrefix = "Next Reward In";
         public string timerTextPostfix = "TIME";
 
+        [Space(25)]
+        [Header("Custom Claim Button")]
+        public bool useCustomClaimButton = false;
+
         [Space(25)] [Header("Buttons Styling Options")]
         public bool useDefaultStyling = true; 
 
         [Space(25)][Header("Redeem Options")]
         
         [Tooltip("allow user to redeem already claimed reward")]
-        public bool useRedeem = false;        
+        public bool useRedeem = false;
+        
+
         
         #region  Singleton
 
